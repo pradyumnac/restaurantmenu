b35178a639963fba49f2e9157fa61444c9aad425
Pradyumna <pady92@gmail.com>
2022-09-28 21:29:39 +0530

    added theme switcher

--- static/scripts.js
+++ static/scripts.js
@@ -18,3 +18,26 @@ btnPrev.addEventListener("click", ()=>{
     currentPageID = nextPageID;
 })
 
+document.getElementById("themeSwitcher").addEventListener("click", toggleTheme)
+
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark'){
+        setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
+    }
+ }
+ // Immediately invoked function to set the theme on initial load
+ (function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+    } else {
+        setTheme('theme-light');
+    }
+ })();
